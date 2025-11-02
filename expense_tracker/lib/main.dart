84e526492f0689dc427626cdaef571a60959ce6b
Alex Bolgan <olex.bolgan@gmail.com>
2025-11-02 16:17:38 +0200

    feat: add provider package and providers

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:expense_tracker/providers/account_provider.dart';
+import 'package:expense_tracker/providers/transaction_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:expense_tracker/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:provider/provider.dart';
 
 
 void main() async {
@@ -10,7 +13,16 @@ void main() async {
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   await FirebaseCrashlytics.instance
       .setCrashlyticsCollectionEnabled(true);
-  runApp(MyApp());
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TransactionProvider()),
+        ChangeNotifierProvider(create: (_) => AccountProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
