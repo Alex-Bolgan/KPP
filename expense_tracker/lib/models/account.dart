d897c73e2131569cb2d3d7dc34335531140758a6
Alex Bolgan <olex.bolgan@gmail.com>
2025-12-06 18:18:48 +0200

    feat: add models and repos

--- expense_tracker/lib/models/account.dart
+++ expense_tracker/lib/models/account.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Account {
+  final String id;
   final String name;
-  final String balance;
-  final IconData icon;
+  final double balance;
+  final String icon;
+  final String userId; // User ID to associate the account with a specific user
 
   Account({
+    required this.id,
     required this.name,
     required this.balance,
     required this.icon,
+    required this.userId,
   });
+
+  factory Account.fromFirestore(Map<String, dynamic> data) {
+    return Account(
+      id: data['id'],
+      name: data['name'],
+      balance: data['balance'],
+      icon: data['icon'],
+      userId: data['userId'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'id': id,
+      'name': name,
+      'balance': balance,
+      'icon': icon,
+      'userId': userId,
+    };
+  }
 }
--- expense_tracker/lib/models/transaction.dart
+++ expense_tracker/lib/models/transaction.dart
@@ -1,21 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Transaction {
-  final String type; // Income or Expense
-  final String amount;
+  final String id;
+  final String type;
+  final double amount;
   final String category;
   final String description;
-  final String wallet;
-  final String date;
-  final IconData icon;
+  final String accountId;
+  final DateTime date;
+  final String icon;
+  final String userId; // User ID to associate the transaction with a specific user
 
   Transaction({
+    required this.id,
     required this.type,
     required this.amount,
     required this.category,
     required this.description,
-    required this.wallet,
+    required this.accountId,
     required this.date,
     required this.icon,
+    required this.userId,
   });
+
+  factory Transaction.fromFirestore(Map<String, dynamic> data) {
+    return Transaction(
+      id: data['id'],
+      type: data['type'],
+      amount: data['amount'],
+      category: data['category'],
+      description: data['description'],
+      accountId: data['accountId'],
+      date: (data['date'] as Timestamp).toDate(),
+      icon: data['icon'],
+      userId: data['userId'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'id': id,
+      'type': type,
+      'amount': amount,
+      'category': category,
+      'description': description,
+      'accountId': accountId,
+      'date': date,
+      'icon': icon,
+      'userId': userId,
+    };
+  }
 }
