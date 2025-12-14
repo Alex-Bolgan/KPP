72fc97dc3107d5ceecf3f6a4bed2c09b5d908ac0
Alex Bolgan <olex.bolgan@gmail.com>
2025-12-14 20:55:44 +0200

    fix: normalize the SDK path

--- expense_tracker/android/settings.gradle.kts
+++ expense_tracker/android/settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
             file("local.properties").inputStream().use { properties.load(it) }
             val flutterSdkPath = properties.getProperty("flutter.sdk")
             require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            flutterSdkPath.replace("\\", "/")
             flutterSdkPath
         }
 
